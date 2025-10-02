91b1971afe4dab3e4e37f24ec17546b937993d14
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-10-02 22:01:13 +0700

    Configure logic for run server

---
+++ src/app.js
@@ -0,0 +1,10 @@
+const createServer = require('./Infrastructures/http/createServer');
+const container = require('./Infrastructures/container');
+
+const start = async () => {
+  const server = await createServer(container);
+  await server.start();
+  console.log(`server start at ${server.info.uri}`);
+};
+
+start();
