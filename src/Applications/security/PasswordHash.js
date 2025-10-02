53548534541001c4709b984567f9e23a4cd7ae73
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-10-02 14:36:56 +0700

    New: password hash

---
+++ src/Applications/security/PasswordHash.js
@@ -0,0 +1,8 @@
+class PasswordHash {
+  /* eslint-disable no-unused-vars */
+  async hash(password) {
+    throw new Error('PASSWORD_HASH.METHOD_NOT_IMPLEMENTED');
+  }
+}
+
+module.exports = PasswordHash;
