de0cecec00eaeee31bb432ed9e3468dce982456d
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-10-02 08:10:09 +0700

    Initial commit

---
+++ eslint.config.mjs
@@ -0,0 +1,24 @@
+import js from "@eslint/js";
+import globals from "globals";
+import { defineConfig } from "eslint/config";
+import daStyle from "eslint-config-dicodingacademy";
+import pluginJest from "eslint-plugin-jest";
+
+export default defineConfig([
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions: { globals: globals.node }
+  },
+  {
+    files: ["**/*.js"],
+    extends: [daStyle],
+    languageOptions: { sourceType: "commonjs" }
+  },
+  {
+    files: ["**/*.test.js"],
+    plugins: { jest: pluginJest },
+    languageOptions: { globals: pluginJest.environments.globals.globals },
+  }
+]);
