b588245a5d22403724334573e2b0a5ba9c276e0f
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-10-04 08:38:50 +0700

    Fix: toThrow jest function

--- src/Domains/users/_test/UserRepository.test.js
+++ src/Domains/users/_test/UserRepository.test.js
@@ -8,7 +8,7 @@ describe('UserRepository interface', () => {
     // Action and Assert
     await expect(userRepository.addUser({})).rejects.toThrow('USER_REPOSITORY.METHOD_NOT_IMPLEMENTED');
     await expect(userRepository.verifyAvailableUsername('')).rejects.toThrow('USER_REPOSITORY.METHOD_NOT_IMPLEMENTED');
-    await expect(userRepository.getPasswordByUsername('')).rejects.toThrowError('USER_REPOSITORY.METHOD_NOT_IMPLEMENTED');
-    await expect(userRepository.getIdByUsername('')).rejects.toThrowError('USER_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+    await expect(userRepository.getPasswordByUsername('')).rejects.toThrow('USER_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+    await expect(userRepository.getIdByUsername('')).rejects.toThrow('USER_REPOSITORY.METHOD_NOT_IMPLEMENTED');
   });
 });
