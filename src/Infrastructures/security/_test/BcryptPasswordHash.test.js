4699f53e2188be718293bd59063b04c72fda3900
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-10-04 11:04:41 +0700

    Implement compare password method

--- src/Infrastructures/security/_test/BcryptPasswordHash.test.js
+++ src/Infrastructures/security/_test/BcryptPasswordHash.test.js
@@ -1,5 +1,6 @@
 const bcrypt = require('bcrypt');
 const BcryptPasswordHash = require('../BcryptPasswordHash');
+const AuthenticationError = require('../../../Commons/exceptions/AuthenticationError');
 
 describe('BcryptPasswordHash', () => {
   describe('hash function', () => {
@@ -15,6 +16,29 @@ describe('BcryptPasswordHash', () => {
       expect(typeof encryptedPassword).toEqual('string');
       expect(encryptedPassword).not.toEqual('plain_password');
       expect(spyHash).toHaveBeenCalledWith('plain_password', 10); // 10 adalah nilai saltRound default untuk BcryptPasswordHash
+    });
+  });
+
+  describe('comparePassword function', () => {
+    it('should throw AuthenticationError if password not match', async () => {
+      // Arrange
+      const bcryptPasswordHash = new BcryptPasswordHash(bcrypt);
+
+      // Act & Assert
+      await expect(bcryptPasswordHash.comparePassword('plain_password', 'encrypted_password'))
+        .rejects
+        .toThrow(AuthenticationError);
+    });
+
+    it('should not return AuthenticationError if password match', async () => {
+      // Arrange
+      const bcryptPasswordHash = new BcryptPasswordHash(bcrypt);
+      const plainPassword = 'secret';
+      const encryptedPassword = await bcryptPasswordHash.hash(plainPassword);
+
+      // Act & Assert
+      await expect(bcryptPasswordHash.comparePassword(plainPassword, encryptedPassword))
+        .resolves.not.toThrow(AuthenticationError);
     });
   });
 });
